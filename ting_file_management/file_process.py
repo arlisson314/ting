94deba82b85e556c970a932d83e8791148cd6242
Arlisson N. Costa <arlisson314@gmail.com>
2023-02-08 22:49:52 -0300

    feat: implementa o metodo process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    index = 0
+    while index < instance.__len__():
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return
+
+    result = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(result),
+        "linhas_do_arquivo": result
+    }
+
+    print(data)
+    instance.enqueue(data)
 
 
 def remove(instance):
