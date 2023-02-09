3d922df016463e54df4a65c6c625ea036f2869eb
Arlisson N. Costa <arlisson314@gmail.com>
2023-02-08 21:46:30 -0300

    style:  corrige formatações

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,10 +1,10 @@
 import sys
 
+
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
         print('Formato inválido', file=sys.stderr)
         return
-
     try:
         with open(path_file, 'r') as file:
             data = file.read()
