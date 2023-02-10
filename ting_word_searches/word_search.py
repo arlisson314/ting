3a70ccbf968eddea7f0ea901e922695f968d3454
Arlisson N. Costa <arlisson314@gmail.com>
2023-02-09 21:32:33 -0300

    feat: implementa o metodo exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    result = []
+    for file in instance.data:
+        lines = []
+        for index, line in enumerate(file['linhas_do_arquivo'], início=0):
+            if word.lower() in line.lower():
+                lines.append(index + 1)
+        if lines:
+            result.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': [{'linha': line_num} for line_num in lines]
+            })
+    return result
 
 
 def search_by_word(word, instance):
